db4f1ccc7cf2c0d894bf3b6c0fe4d9eeca827cf0
Abhishek Kumar Singh <abhishek99.aks@gmail.com>
2023-09-06 22:47:46 +0530

    Completed Add Expense Overlay

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:expensestracker/widgets/expenses_list/expenses_list.dart';
+import 'package:expensestracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 import 'package:expensestracker/models/expense.dart';
 
@@ -29,14 +30,55 @@ class _ExpensesState extends State<Expenses> {
 
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
-        context: context,
-        builder: (ctx) {
-          return const Text('Model Bottom Sheet');
-        });
+      isScrollControlled: true,
+      context: context,
+      builder: (ctx) {
+        return NewExpense(
+          onAddedExpense: _addExpense,
+        );
+      },
+    );
+  }
+
+  void _addExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.add(expense);
+    });
+  }
+
+  void _removeExpense(Expense expense) {
+    final expenseIndex = _registeredExpenses.indexOf(expense);
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 3),
+        content: const Text('Expense deleted'),
+        action: SnackBarAction(
+          label: 'Undo',
+          onPressed: () {
+            setState(() {
+              _registeredExpenses.insert(expenseIndex, expense);
+            });
+          },
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(context) {
+    Widget mainContent = const Center(child: Text('No Expenses Found'));
+
+    if (_registeredExpenses.isNotEmpty) {
+      mainContent = ExpensesList(
+        expenses: _registeredExpenses,
+        onRemoveExpense: _removeExpense,
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Expense Tracker'),
@@ -51,9 +93,8 @@ class _ExpensesState extends State<Expenses> {
         children: [
           const Text('data'),
           Expanded(
-              child: ExpensesList(
-            expenses: _registeredExpenses,
-          ))
+            child: mainContent,
+          )
         ],
       ),
     );
